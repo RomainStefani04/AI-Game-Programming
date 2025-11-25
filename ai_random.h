3b3036f9165317e7785a68403273046fb108afc2
Romain <romain.stefani@etu.unice.fr>
2025-11-25 14:31:28 +0100

    Ajout du jeu (structure et moteur de jeu), ajout d'une premiere version random, d'un main et du Readme

---
+++ ai_random.h
@@ -0,0 +1,12 @@
+//
+// Created by romai on 24/11/2025.
+//
+
+#ifndef AI_RANDOM_H
+#define AI_RANDOM_H
+
+#include "game.h"
+
+void ai_random_move(const GameState *state, Move *selected_move);
+
+#endif //AI_RANDOM_H
